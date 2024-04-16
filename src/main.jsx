f9c42399a9eba5b5a6bd224e487d169f0f8bae96
isamanzano <isabelacmanzano05@gamil.com>
2024-04-16 16:09:47 -0300

    hoje

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MainRoutes from "./route.jsx"
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from "react-dom/client"
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
