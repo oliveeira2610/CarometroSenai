5bd52a63ce68517c7aa12d6e3064e788b716d020
oliveeira2610 <bielolirodrigues@gmail.com>
2024-04-16 16:26:45 -0300

    hoje

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import Image from 'react-bootstrap/Image'; 
-import './home.css'; 
+import React from "react";
+import Image from "react-bootstrap/Image";
+import "./home.css";
 
 function Home() {
   return (
 
-
     <div className="Tela-Home">
-      <header>
-        <div>
-        <img className='senailogo' src="https://desenvolveitapevi.wordpress.com/wp-content/uploads/2016/02/logo-senai1.png"  />
-        </div>
+      <header className="header">
+        <img
+          className="senailogo"
+          src="https://desenvolveitapevi.wordpress.com/wp-content/uploads/2016/02/logo-senai1.png"
+        />
       </header>
 
 
-      <img className="imagem-lateral" src="https://live.staticflickr.com/7601/16996079569_dbd29856a7_b.jpg" fluid/>
+      <div className="imagem-div">
+        <img
+          className="imagem-lateral"
+          src="https://live.staticflickr.com/7601/16996079569_dbd29856a7_b.jpg"
+          fluid
+        />
+      </div>
 
-
-   
     </div>
   );
 }
--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,27 +1,29 @@
-import './login.css';
-import { Link } from 'react-router-dom';
+import "./login.css";
+import { Link } from "react-router-dom";
 function Login() {
-
-
   return (
-
-    <div>
-      <form id="msform">
-        <fieldset>
-          <h2 className="fs-title">Login</h2>
-          <h3 className="fs-subtitle">Insira seus dados para Login</h3>
-          <input type="text" name="email" placeholder="Email" />
-          <input type="password" name="pass" placeholder="Senha" />
-          <h4>Não tem uma conta?</h4>
-          <div className='botoes'>
-          <Link to="/cadastro" className="action-button">Cadastre-se</Link>
-          <Link to="/home" className="action-button">Login</Link>
-          </div>
-        </fieldset>
-      </form>
+    <div className="geral">
+      <div>
+        <form id="msform">
+          <fieldset>
+            <h2 className="fs-title">Login</h2>
+            <h3 className="fs-subtitle">Insira seus dados para Login</h3>
+            <input type="text" name="email" placeholder="Email" />
+            <input type="password" name="pass" placeholder="Senha" />
+            <h4>Não tem uma conta?</h4>
+            <div className="botoes">
+              <Link to="/cadastro" className="action-button">
+                Cadastre-se
+              </Link>
+              <Link to="/home" className="action-button">
+                Login
+              </Link>
+            </div>
+          </fieldset>
+        </form>
+      </div>
     </div>
-
-  )
+  );
 }
 
 export default Login;
