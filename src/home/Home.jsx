fb1ab990d4f0dc4e3dee9d3a3c7049356d5302db
oliveeira2610 <bielolirodrigues@gmail.com>
2024-04-16 15:06:05 -0300

    commit2

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -4,20 +4,21 @@ import './home.css';
 
 function Home() {
   return (
-    
+
+
     <div className="Tela-Home">
       <header>
         <div>
         <img className='senailogo' src="https://desenvolveitapevi.wordpress.com/wp-content/uploads/2016/02/logo-senai1.png"  />
         </div>
       </header>
-    
+
+
       <img className="imagem-lateral" src="https://live.staticflickr.com/7601/16996079569_dbd29856a7_b.jpg" fluid/>
-    
-    
+
+
    
     </div>
   );
 }
-
 export default Home;
