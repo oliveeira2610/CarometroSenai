0f77923ac217f6712bd76af7da0b7fc42a343319
isamanzano <isabelacmanzano05@gamil.com>
2024-04-16 17:21:01 -0300

    commit com sidebar

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -3,8 +3,22 @@ import Image from "react-bootstrap/Image";
 import "./home.css";
 
 function Home() {
-  return (
+  var dropdown = document.getElementsByClassName("dropdown-btn");
+  var i;
 
+  for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  }
+
+  return (
     <div className="Tela-Home">
       <header className="header">
         <img
@@ -13,6 +27,24 @@ function Home() {
         />
       </header>
 
+      <div className="sidebar">
+        <div class="sidenav">
+          <a href="#about">About</a>
+          <a href="#services">Services</a>
+          <a href="#clients">Clients</a>
+          <a href="#contact">Contact</a>
+          <button class="dropdown-btn">
+            Dropdown
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-container">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+          <a href="#contact">Search</a>
+        </div>
+      </div>
 
       <div className="imagem-div">
         <img
@@ -21,7 +53,6 @@ function Home() {
           fluid
         />
       </div>
-
     </div>
   );
 }
