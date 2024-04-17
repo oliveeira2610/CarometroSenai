e494d985df25abc472f4628d656a0b19e1602aa0
oliveeira2610 <bielolirodrigues@gmail.com>
2024-04-17 15:20:32 -0300

    foii

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,22 +1,12 @@
-import React from "react";
-import Image from "react-bootstrap/Image";
-import "./home.css";
+import React, { useState } from "react";
+import "./home.css"; // Importe os estilos CSS da página inicial
 
 function Home() {
-  var dropdown = document.getElementsByClassName("dropdown-btn");
-  var i;
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
-  for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function () {
-      this.classList.toggle("active");
-      var dropdownContent = this.nextElementSibling;
-      if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-      } else {
-        dropdownContent.style.display = "block";
-      }
-    });
-  }
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
 
   return (
     <>
@@ -29,37 +19,34 @@ function Home() {
           <img
             className="senailogo"
             src="https://desenvolveitapevi.wordpress.com/wp-content/uploads/2016/02/logo-senai1.png"
+            alt="Senai Logo"
           />
         </header>
-
-        <div class="sidenav">
+        <div className={`sidenav ${dropdownOpen ? 'active' : ''}`}>
           <a href="#about">About</a>
           <a href="#services">Services</a>
           <a href="#clients">Clients</a>
           <a href="#contact">Contact</a>
-          <button class="dropdown-btn">
-            Dropdown
-            <i class="fa fa-caret-down"></i>     
+          <button className="dropdown-btn" onClick={toggleDropdown}>Dropdown 
+            <i className={`fa fa-caret-down ${dropdownOpen ? 'active' : ''}`}></i>
           </button>
-          <div class="dropdown-container">
+          <div className={`dropdown-container ${dropdownOpen ? 'show' : ''}`}>
             <a href="#">Link 1</a>
             <a href="#">Link 2</a>
             <a href="#">Link 3</a>
           </div>
           <a href="#contact">Search</a>
         </div>
-
-      
-
         <div className="imagem-div">
           <img
             className="imagem-lateral"
             src="https://live.staticflickr.com/7601/16996079569_dbd29856a7_b.jpg"
-            fluid
+            alt="Imagem Lateral"
           />
         </div>
       </div>
     </>
   );
 }
+
 export default Home;
