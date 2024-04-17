1865e13fdfc444598add720a6577e6b1bfdf5ca2
oliveeira2610 <bielolirodrigues@gmail.com>
2024-04-17 17:12:40 -0300

    commit

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -29,7 +29,8 @@ function Home() {
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
       ></link>
-      <div className="Tela-Home">
+
+      <div className="TelHomea-">
         <header className="header">
           <img
             className="senailogo"
@@ -38,43 +39,67 @@ function Home() {
           />
         </header>
 
-        <div className="sidenav">
-          <a href="#about"></a>
-          
-          <button className="dropdown-btn" onClick={toggleDropdownHome}>Home
-            <i className={`fa fa-caret-down ${dropdownOpenHome ? 'active' : ''}`}></i>
-          </button>
-          <div className={`dropdown-container ${dropdownOpenHome ? 'show' : ''}`}>
-            <a href="#">Tirar Foto</a>
-            <a href="#">Consultas</a>
-          </div>
-
-          <button className="dropdown-btn" onClick={toggleDropdownUsuarios}>Usuários
-            <i className={`fa fa-caret-down ${dropdownOpenUsuarios ? 'active' : ''}`}></i>
-          </button>
-          <div className={`dropdown-container ${dropdownOpenUsuarios ? 'show' : ''}`}>
-            <a href="#">Cursos</a>
-            <a href="#">Turmas</a>
-          </div>
-
-          <button className="dropdown-btn" onClick={toggleDropdownAluno}>Aluno 
-            <i className={`fa fa-caret-down ${dropdownOpenAluno ? 'active' : ''}`}></i>
-          </button>
-          <div className={`dropdown-container ${dropdownOpenAluno ? 'show' : ''}`}>
-            <a href="#">Ocorrências</a>
-            <a href="#">Anexos</a>
-          </div>
-
-          <a href="matricula">Mátricula</a>
-
-
-        </div>
-        <div className="imagem-div">
+        <div className="conteudo">
           <img
             className="imagem-lateral"
             src="https://live.staticflickr.com/7601/16996079569_dbd29856a7_b.jpg"
             alt="Imagem Lateral"
           />
+
+          <div className="sidenav">
+            <a href="#about"></a>
+
+            <button className="dropdown-btn" onClick={toggleDropdownHome}>
+              Home
+              <i
+                className={`fa fa-caret-down ${
+                  dropdownOpenHome ? "active" : ""
+                }`}
+              ></i>
+            </button>
+            <div
+              className={`dropdown-container ${dropdownOpenHome ? "show" : ""}`}
+            >
+              <a href="#">Tirar Foto</a>
+              <a href="#">Consultas</a>
+            </div>
+
+            <button className="dropdown-btn" onClick={toggleDropdownUsuarios}>
+              Usuários
+              <i
+                className={`fa fa-caret-down ${
+                  dropdownOpenUsuarios ? "active" : ""
+                }`}
+              ></i>
+            </button>
+            <div
+              className={`dropdown-container ${
+                dropdownOpenUsuarios ? "show" : ""
+              }`}
+            >
+              <a href="#">Cursos</a>
+              <a href="#">Turmas</a>
+            </div>
+
+            <button className="dropdown-btn" onClick={toggleDropdownAluno}>
+              Aluno
+              <i
+                className={`fa fa-caret-down ${
+                  dropdownOpenAluno ? "active" : ""
+                }`}
+              ></i>
+            </button>
+            <div
+              className={`dropdown-container ${
+                dropdownOpenAluno ? "show" : ""
+              }`}
+            >
+              <a href="#">Ocorrências</a>
+              <a href="#">Anexos</a>
+            </div>
+
+            <a href="matricula">Mátricula</a>
+          </div>
         </div>
       </div>
     </>
