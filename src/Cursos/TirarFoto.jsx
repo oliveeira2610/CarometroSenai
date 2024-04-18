21c72cb52e52a8a7b15f8854d6f01d43596fbaa3
isamanzano <isabelacmanzano05@gamil.com>
2024-04-18 13:52:13 -0300

    hj

---
+++ src/Cursos/TirarFoto.jsx
@@ -0,0 +1,28 @@
+import "/.tirarfoto.css";
+import Nav from 'react-bootstrap/Nav';
+
+function TirarFoto() {
+  return (
+    <Nav
+      activeKey="/TirarFoto"
+      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+    >
+      <Nav.Item>
+        <Nav.Link href="/home">Active</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="disabled" disabled>
+          Disabled
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
+}
+
+export default TirarFoto;
