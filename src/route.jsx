2bba528416a55e96cf7c6d50114684ac9a3272c2
oliveeira2610 <bielolirodrigues@gmail.com>
2024-04-18 14:20:43 -0300

    commit

--- src/route.jsx
+++ src/route.jsx
@@ -2,13 +2,15 @@ import {Routes, Route} from "react-router-dom";
 import Login from "./login/Login.jsx";
 import Cadastro from "./cadastro/Cadastro.jsx";
 import Home from "./home/Home.jsx"
+import Tirarfoto from "./tirarfoto/TirarFoto.jsx"
 
 function MainRoutes(){
     return(
         <Routes>
             <Route path="/" element={<Login/>}/>
             <Route path="/cadastro" element={<Cadastro/>}/>
-            <Route path="/home" element={<Home/>}/> {/* Adicionando rota para Home */}
+            <Route path="/home" element={<Home/>}/> 
+            <Route path="/tirarfoto" element={<Tirarfoto/>}/>
         </Routes>
     )
 }
