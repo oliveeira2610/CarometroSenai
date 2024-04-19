4cc6a69054f03e6fcd1192e47bc12dd99496103b
oliveeira2610 <bielolirodrigues@gmail.com>
2024-04-19 07:41:26 -0300

    commit

--- src/tirarfoto/TirarFoto.jsx
+++ src/tirarfoto/TirarFoto.jsx
@@ -20,7 +20,11 @@ function TirarFoto() {
       onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
     >
       <Nav.Item>
-        <Nav.Link href="/Home">Home</Nav.Link>
+        <Nav.Link className="navbarbutton" href="/Home">Home</Nav.Link>
+        ,
+
+
+
       </Nav.Item>
       <Nav.Item>
         <Nav.Link eventKey="link">Usuários</Nav.Link>
