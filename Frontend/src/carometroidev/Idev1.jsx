8dcc4260c8cddbfa80217d313159ac40ae2e0cca
isamanzano <isabelacmanzano05@gamil.com>
2024-05-06 14:43:40 -0300

    h

--- Frontend/src/carometroidev/Idev1.jsx
+++ Frontend/src/carometroidev/Idev1.jsx
@@ -1,75 +1,87 @@
-import "./carometroidev.css";
+import React from 'react';
 import { Link } from "react-router-dom";
+import "./carometroidev.css";
 
 function Idev1() {
-    return (
-      <>
-        <header>
-          
+  return (
+    <>
+      <header>
         <img className="senailogo"
-              src="https://desenvolveitapevi.wordpress.com/wp-content/uploads/2016/02/logo-senai1.png"
-              alt="Senai Logo"
-            />
-  
-          <div className="navbartop">
-            <Link to="/home" className="navbarbutton">
-              Home
-            </Link>
-  
-            <Link to="/" className="navbarbutton">
-              Usuários
-            </Link>
-  
-            <Link to="/cursos" className="navbarbutton">
-              Cursos
-            </Link>
-          </div>
-        </header>
-        <div class="cards-list">
-      <div className="card 1">
-
-      <Link to="/carometroidev1" className="botão-card-idev">
-        <div className="card_image_card" >
-          <img className="card_image_card" src="src/assets/idev1.jpg" alt="IDEV1" />
+          src="https://desenvolveitapevi.wordpress.com/wp-content/uploads/2016/02/logo-senai1.png"
+          alt="Senai Logo"
+        />
+        <div className="navbartop">
+          <Link to="/home" className="navbarbutton">
+            Home
+          </Link>
+          <Link to="/" className="navbarbutton">
+            Usuários
+          </Link>
+          <Link to="/cursos" className="navbarbutton">
+            Cursos
+          </Link>
         </div>
-      </Link>
-        <div className="card_title title-white">
-          <p>IDEV1 - 2022</p>
-        </div>
-    </div>
-
-      <div class="card 2">
-      <Link to="/carometroidev2" className="botão-card">
-        <div className="card_image_card">
-          <img className="card_image_card"   src="src/assets/idev2.jpg" />
-        </div>
-      </Link>
-      <div class="card_title title-white">
-          <p>IDEV2 - 2023</p>
-      </div>
-      </div>
+      </header>
       
-          <div class="card 3">
-      <Link to="/carometroidev3" className="botão-card" >
-        <div className="card_image_card">
-          <img className="card_image_card" src="https://cdn-icons-png.flaticon.com/512/4123/4123763.png" />
-          <div class="overlay">
-            <div class="text">Nome: João<br/>Telefone: (11) 99999-9999<br/>CPF: 123.456.789-00</div>
+      <div className="cards-list">
+        <div className="card card-1">
+          <Link to="/" className="botao-card">
+            <div className="card_image_card">
+              <img className="card_image" src="https://cdn-icons-png.flaticon.com/512/4123/4123763.png" alt="User Icon"/>
+              <div className="overlay">
+                <div className="text">Nome: João<br/>Telefone: (11) 99999-9999<br/>CPF: 123.456.789-00</div>
+              </div>
+            </div>
+          </Link>
+          <div className="card_title title-white">
+            <p>Alexandre</p>
           </div>
         </div>
-      </Link>
-      <div class="card_title title-white">
-        <p>IDEV3 - 2024</p>
+
+        <div className="card card-2">
+          <Link to="/" className="botao-card">
+            <div className="card_image_card">
+              <img className="card_image" src="https://cdn-icons-png.flaticon.com/512/4123/4123763.png" alt="User Icon"/>
+              <div className="overlay">
+                <div className="text">Nome: João<br/>Telefone: (11) 99999-9999<br/>CPF: 123.456.789-00</div>
+              </div>
+            </div>
+          </Link>
+          <div className="card_title title-white">
+            <p>Alexandre</p>
+          </div>
+        </div>
+
+        <div className="card card-3">
+          <Link to="/" className="botao-card">
+            <div className="card_image_card">
+              <img className="card_image" src="https://cdn-icons-png.flaticon.com/512/4123/4123763.png" alt="User Icon"/>
+              <div className="overlay">
+                <div className="text">Nome: João<br/>Telefone: (11) 99999-9999<br/>CPF: 123.456.789-00</div>
+              </div>
+            </div>
+          </Link>
+          <div className="card_title title-white">
+            <p>Alexandre</p>
+          </div>
+        </div>
+
+        <div className="card card-4">
+          <Link to="/" className="botao-card">
+            <div className="card_image_card">
+              <img className="card_image" src="https://cdn-icons-png.flaticon.com/512/4123/4123763.png" alt="User Icon"/>
+              <div className="overlay">
+                <div className="text">Nome: João<br/>Telefone: (11) 99999-9999<br/>CPF: 123.456.789-00</div>
+              </div>
+            </div>
+          </Link>
+          <div className="card_title title-white">
+            <p>Alexandre</p>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
+  );
+}
 
-
-
-        
-      </div>
-      </>
-    );
-  }
-  
-  export default Idev1;
-  
+export default Idev1;
