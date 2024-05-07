1513a005689bc998844ddadd43c4aede4033c7c1
oliveeira2610 <bielolirodrigues@gmail.com>
2024-05-07 13:45:50 -0300

    commit

--- Frontend/src/carometroiele/Iele3.jsx
+++ Frontend/src/carometroiele/Iele3.jsx
@@ -6,10 +6,12 @@ function Iele3() {
   return (
     <>
       <header>
+      <Link to="/home" className="link-senai-logo">
         <img className="senailogo"
-          src="https://desenvolveitapevi.wordpress.com/wp-content/uploads/2016/02/logo-senai1.png"
-          alt="Senai Logo"
-        />
+            src="https://desenvolveitapevi.wordpress.com/wp-content/uploads/2016/02/logo-senai1.png"
+            alt="Senai Logo"
+          />
+          </Link>
         <div className="navbartop">
           <Link to="/home" className="navbarbutton">
             Home
