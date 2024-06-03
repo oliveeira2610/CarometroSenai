6cb111cce32fbbeefc9b9dd170d0a4db81bc1739
isamanzano <isabelacmanzano05@gamil.com>
2024-06-03 16:49:18 -0300

    g

--- Frontend/src/usuarios/Usuarios.jsx
+++ Frontend/src/usuarios/Usuarios.jsx
@@ -25,6 +25,29 @@ function Usuarios() {
         </div>
       </header>
 
+      <div class="cards-list">
+      <div className="card 1">
+      <Link to="/tirarfoto" className="botão-card-usuario1">
+        <div className="card_image_usuario1" >
+          <img className="card_image_usuario1" src="https://cdn-icons-png.flaticon.com/512/44/44413.png" />
+        </div>
+      </Link>
+        <div className="card_title title-white">
+          <p>Tirar Foto</p>
+        </div>
+    </div>
+
+      <div class="card 2">
+      <Link to="/consultas" className="botão-card-usuario2">
+        <div className="card_image_usuario2">
+          <img className="card_image_usuario2" src="https://awmselos.com.br/wp-content/uploads/2016/04/icone-quem-somos.png" />
+        </div>
+      </Link>
+      <div className="card_title title-white">
+          <p>Consultas</p>
+      </div>
+      </div>
+        </div>
 
     </>
 
