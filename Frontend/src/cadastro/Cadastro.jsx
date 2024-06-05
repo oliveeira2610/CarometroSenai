51ed556413b248a9eef3744d5643897000a639db
oliveeira2610 <bielolirodrigues@gmail.com>
2024-06-05 15:21:27 -0300

    i

--- Frontend/src/cadastro/Cadastro.jsx
+++ Frontend/src/cadastro/Cadastro.jsx
@@ -1,24 +1,19 @@
-import React, { useState } from "react"; // Importa o React e a função useState do React
-import "./cadastro.css"; // Importa o arquivo CSS de estilos específicos para esta página
-import { Link } from "react-router-dom"; // Importa o componente Link do react-router-dom
-
+import React, { useState } from "react";
+import "./cadastro.css";
+import { Link } from "react-router-dom";
 function Cadastro() {
-  // Define um componente de função chamado Cadastro
+  const [step, setStep] = useState(1);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [cpass, setCpass] = useState("");
+  const [cep, setCep] = useState("");
+  const [rua, setRua] = useState("");
+  const [bairro, setBairro] = useState("");
+  const [cidade, setCidade] = useState("");
+  const [estado, setEstado] = useState("");
+  const [numero, setNumero] = useState("");
 
-  // Define os estados iniciais usando o hook useState
-  const [step, setStep] = useState(1); // Estado para controlar o passo do formulário
-  const [name, setName] = useState(""); // Estado para armazenar o nome
-  const [email, setEmail] = useState(""); // Estado para armazenar o email
-  const [pass, setPass] = useState(""); // Estado para armazenar a senha
-  const [cpass, setCpass] = useState(""); // Estado para armazenar a confirmação de senha
-  const [cep, setCep] = useState(""); // Estado para armazenar o CEP
-  const [rua, setRua] = useState(""); // Estado para armazenar a rua
-  const [bairro, setBairro] = useState(""); // Estado para armazenar o bairro
-  const [cidade, setCidade] = useState(""); // Estado para armazenar a cidade
-  const [estado, setEstado] = useState(""); // Estado para armazenar o estado
-  const [numero, setNumero] = useState(""); // Estado para armazenar o número
-
-  // Função para lidar com as mudanças nos campos de formulário
   const handleChange = (e) => {
     const { name, value } = e.target;
     switch (name) {
@@ -57,79 +52,196 @@ function Cadastro() {
     }
   };
 
-  // Função para avançar para o próximo passo do formulário
   const nextStep = () => {
     setStep(step + 1);
   };
 
-  // Função para voltar para o passo anterior do formulário
   const prevStep = () => {
     setStep(step - 1);
   };
 
-  // Função para lidar com o envio do formulário
   const handleSubmit = (e) => {
     e.preventDefault();
     // Aqui você pode adicionar a lógica para enviar os dados do formulário
   };
 
-  // Retorna o JSX que representa a estrutura do formulário de cadastro
+
+
   return (
     <div className="geral-cadastro">
-      {/* Formulário */}
       <form id="msform" onSubmit={handleSubmit}>
         
-        {/* Logo SENAI */}
-        <img
-          className="senailogocadastro"
-          src="https://upload.wikimedia.org/wikipedia/commons/8/8c/SENAI_S%C3%A3o_Paulo_logo.png"
-          alt="Logo do SENAI"
-        />
+      <img
+        className="senailogocadastro"
+        src="https://upload.wikimedia.org/wikipedia/commons/8/8c/SENAI_S%C3%A3o_Paulo_logo.png"
+        alt="Logo do SENAI"
+      />
 
-        {/* Barra de progresso */}
         <ul id="progressbar">
           <li className={step >= 1 ? "active" : ""}>Cadastro</li>
           <li className={step >= 2 ? "active" : ""}>Endereço</li>
           <li className={step >= 3 ? "active" : ""}>Suas informações</li>
         </ul>
 
-        {/* Renderização condicional com base no passo atual */}
         {step === 1 && (
-          // Primeiro passo do formulário
           <fieldset>
             <h2 className="fs-title">Cadastro</h2>
             <h3 className="fs-subtitle">Insira seus dados para cadastro</h3>
-            {/* Inputs para nome, email, senha e confirmação de senha */}
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={handleChange}
+              placeholder="Nome"
+            />
+            <input
+              type="text"
+              name="email"
+              value={email}
+              onChange={handleChange}
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              name="pass"
+              value={pass}
+              onChange={handleChange}
+              placeholder="Senha"
+            />
+            <input
+              type="password"
+              name="cpass"
+              value={cpass}
+              onChange={handleChange}
+              placeholder="Confirmar senha"
+            />
+            <div className="botoes">
+              <Link to="/" className="action-button">
+                Voltar
+              </Link>
+              <input
+                type="button"
+                onClick={nextStep}
+                className="next action-button"
+                value="Próximo"
+              />
+            </div>
           </fieldset>
         )}
 
         {step === 2 && (
-          // Segundo passo do formulário
           <fieldset>
             <h2 className="fs-title">Endereço</h2>
             <h3 className="fs-subtitle">Insira seu Endereço</h3>
-            {/* Inputs para CEP, rua, bairro, cidade, estado e número */}
+            <input
+              type="text"
+              name="cep"
+              value={cep}
+              onChange={handleChange}
+              placeholder="Cep"
+            />
+            <input
+              type="text"
+              name="rua"
+              value={rua}
+              onChange={handleChange}
+              placeholder="Rua"
+            />
+            <input
+              type="text"
+              name="bairro"
+              value={bairro}
+              onChange={handleChange}
+              placeholder="Bairro"
+            />
+            <input
+              type="text"
+              name="cidade"
+              value={cidade}
+              onChange={handleChange}
+              placeholder="Cidade"
+            />
+            <input
+              type="text"
+              name="estado"
+              value={estado}
+              onChange={handleChange}
+              placeholder="Estado"
+            />
+            <input
+              type="text"
+              name="numero"
+              value={numero}
+              onChange={handleChange}
+              placeholder="Numero"
+            />
+            <div className="botoes">
+              <input
+                type="button"
+                onClick={prevStep}
+                className="previous action-button"
+                value="Voltar"
+              />
+              <input
+                type="button"
+                onClick={nextStep}
+                className="next action-button"
+                value="Próximo"
+              />
+            </div>
           </fieldset>
         )}
 
         {step === 3 && (
-          // Terceiro passo do formulário
           <fieldset>
             <h2 className="fs-title">Resumo</h2>
             <h3 className="fs-subtitle">
               Aqui estão as informações que você inseriu:
             </h3>
-            {/* Exibição das informações inseridas */}
+            <p id="resumo" className="resumo-texto">
+              Nome: {name}
+              <br />
+              Email: {email}
+              <br />
+              CEP: {cep}
+              <br />
+              Rua: {rua}
+              <br />
+              Bairro: {bairro}
+              <br />
+              Cidade: {cidade}
+              <br />
+              Estado: {estado}
+              <br />
+              Número: {numero}
+            </p>
+            <div className="botoes">
+              <input
+                type="button"
+                onClick={prevStep}
+                className="previous action-button"
+                value="Voltar"
+              />
+              <input
+                type="submit"
+                className="submit action-button"
+                value="Enviar"
+              />
+            </div>
           </fieldset>
         )}
-
-        {/* Botões de navegação entre os passos */}
       </form>
-      
-      {/* Animação de ondas no fundo */}
       <div className="waveWrapper waveAnimation">
-        {/* Camadas de ondas */}
-      </div>
+          <div className="waveWrapperInner bgTop">
+            <div className="wave waveTop" style={{ backgroundImage: "url('http://front-end-noobs.com/jecko/img/wave-top.png')" }}></div>
+          </div>
+          <div className="waveWrapperInner bgMiddle">
+            <div className="wave waveMiddle" style={{ backgroundImage: "url('http://front-end-noobs.com/jecko/img/wave-mid.png')" }}></div>
+          </div>
+          <div className="waveWrapperInner bgBottom">
+            <div className="wave waveBottom" style={{ backgroundImage: "url('http://front-end-noobs.com/jecko/img/wave-bot.png')" }}></div>
+          </div>
+        </div>
     </div>
   );
 }
